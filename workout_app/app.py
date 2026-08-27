f85d53b69b875fe1e40ffead5c1cf7a429823e0b
Ben Pearson <benpearson@Bens-MacBook-Air.local>
2026-08-27 14:42:21 -0600

    html and css, we have a working webapp now :D

--- workout_app/app.py
+++ workout_app/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from services.randomizer import generate_week
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Upper Body Randomizer is running!"
+    week = generate_week()
+
+    return render_template(
+        "week.html",
+        week=week
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
